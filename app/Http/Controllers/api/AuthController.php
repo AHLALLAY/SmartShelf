3befbcb0235b9f5a3301d6d4411f83c425127fcb
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-11 13:12:19 +0000

    feat: Ajouter la fonction d'inscription

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -9,5 +9,28 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validated_data = $request->validate([
+            'name' => ['required', 'string', 'max:30'],
+            'email' => ['required', 'string', 'unique:users,email', 'email', 'max:50'],
+            'password' => ['required', 'string', 'min:6'],
+            'roles' => ['required', 'string', 'in:client,admin'],
+        ]);
+
+        $validated_data['password'] = Hash::make($validated_data['password']);
+
+        $user = User::create($validated_data);
+        if ($user) {
+            return response()->json([
+                'message' => 'User created successfully!'
+            ], 201);
+        } else {
+            return response()->json([
+                'message' => 'Unexpected error occurred.'
+            ], 500);
+        }
+    }
+
 
 }
