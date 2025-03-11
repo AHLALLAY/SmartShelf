cb801048bfc7edc653d95f226db2d867c9b6bda9
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-11 13:11:18 +0000

    feat: Ajouter le controlleur d'authentification

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+}
