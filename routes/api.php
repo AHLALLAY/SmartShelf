ae4eb7dfc73fd587526b4cad60b41c27a5aab961
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-13 10:27:20 +0000

    feat: Ajouter la method 'search' avec sa route

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,9 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::post('/product/create', [ProductController::class, 'createProduct'])->name('new-product');
-Route::get('/product/display/available', [ProductController::class, 'displayProductAvailable'])->name('available-product');
+Route::post('/product/create', [ProductController::class, 'createProduct'])->name('new.product');
+Route::get('/product/display/available', [ProductController::class, 'displayProductAvailable'])->name('available.product');
 Route::post('/product/sale/{id}', [ProductController::class, 'saleProduct']);
-Route::get('/product/display/populare', [ProductController::class, 'displayProductPopulare'])->name('populare-product');
-Route::get('/product/display/promotion', [ProductController::class, 'displayProductPromo'])->name('promotion-product');
+Route::get('/product/display/populare', [ProductController::class, 'displayProductPopulare'])->name('populare.product');
+Route::get('/product/display/promotion', [ProductController::class, 'displayProductPromo'])->name('promotion.product');
+Route::get('/products/search', [ProductController::class, 'search'])->name('search.product');
